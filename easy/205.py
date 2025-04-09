2b982e64b887696d38604a52ae2f60a61a7c96e8
Abraham Cruz <abrahamcz_@hotmail.com>
2025-04-08 19:57:18 -0600

    is isomorphic

---
+++ easy/205.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        map_s_t = {}
+        map_t_s = {}
+        
+        for char_s, char_t in zip(s, t):
+            if (char_s in map_s_t and map_s_t[char_s] != char_t) or \
+                (char_t in map_t_s and map_t_s[char_t] != char_s):
+                return False
+
+            map_s_t[char_s] = char_t
+            map_t_s[char_t] = char_s
+
+        return True
+        
+        
+solution = Solution()
+
+s = "edd"
+t = "add"
+result: bool = solution.isIsomorphic(s, t)
+
+print(result)
